b7783b35ed69e9a1e0c4f09e0f081bd01ae74f37
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-05 18:36:29 +0300

    Implement code changes to enhance functionality and improve performance

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const { MONGO_URL } = require("../util/config");
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+if (MONGO_URL && !mongoose.connection.readyState)
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to ", MONGO_URL);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
 
 module.exports = {
-  Todo
-}
+  Todo,
+};
