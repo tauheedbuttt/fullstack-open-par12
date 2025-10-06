3f417042f24f80825c1d0a4ac77931f7cb65af7b
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-06 11:06:36 +0300

    feat(phonebook): dev on container

--- phonebook/frontend/src/services/persons.js
+++ phonebook/frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "/api/persons";
+const baseUrl = `${import.meta.env.VITE_BACKEND_URL}/api/persons`;
 
 const getAll = () => {
   const promise = axios.get(baseUrl);
--- phonebook/frontend/vite.config.js
+++ phonebook/frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 8080, // This is the port users connect to (nginx port)
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+});
